21b20a9f75f167f47b2326800c3b69237b3449f2
Emode Ohimai <67082364+ohis-coder@users.noreply.github.com>
2026-03-25 14:28:22 +0100

    back again, i dont even know what to say.. this is the most interesting thing i have done in my life no single ai code on there let me jus update you and go to bed now i am exhausted - so on the last commit i created the bla bla bla 3 files and pre calculated the sizing... ALL WRONG - so now i used a macro to calculate - i created another id.bin file to attach a user to an id sequentially - i started doing pointer maths even tho i have lil experience w c, ps; they lied it is not hard at all.. just & and * easy, skill issue.. jk - so the main thingy here was the write_to_db fuction here and this was not even the hardest part.. so i used the getchar i learnt from K&R i think it is in my c_projects repo... - ... i used the pointer to know where i am starting to write from and where to end and fill it up with \0 so that i pad the extra space with \0's - i then leave one \0 for padding - i increment the pointer to keep writing on the space available - simple - now this was the draining part, the number of boilerplate.. i had so much copy and paste to do cause i was setting ftruncate, open, munmap, mmap, mapfail on every file UGHHHHHHHH - in the end mahn, i got the db i wanted.. not the one i need.. - i might write an issue or something but you read through it - i need to increase the db size when it is filled to accomodate more than 256 users - i need to write the read from tdb for the user id and make the user id require a password OMFG more boilerplate god, why me! - i need to relate the database, well that is already done - mahn that read will cost a lot on the ram cause we are basically lifting like 64 user names to get one and so on, dont care RAS CAS is not my problem, skill issue, get a gpu or sth

--- create_account_v2/create_account.c
+++ create_account_v2/create_account.c
@@ -1,39 +1,71 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/fcntl.h>
 #include <sys/mman.h>
 #include <unistd.h>
 
 #define FAILED -1
-#define STRIDE_PHONE 8
-#define STRIDE_NAME 16
+#define STRIDE_ID 8
+#define STRIDE_PHONE 16
+#define STRIDE_NAME 64
 #define STRIDE_ADDR 128
-#define PAGE_SIZE 4096
+#define TOTAL_USERS 256
+#define ID_PAGE_SIZE STRIDE_ID *TOTAL_USERS
+#define NAME_PAGE_SIZE STRIDE_NAME *TOTAL_USERS
+#define PHONE_PAGE_SIZE STRIDE_PHONE *TOTAL_USERS
+#define ADDRESS_PAGE_SIZE STRIDE_ADDR *TOTAL_USERS
+
+int id = 0;
+
+void write_to_db(char *base_pointer, int id, int stride) {
+  int c;
+  memset(&base_pointer[id * stride], '\0', stride);
+  int length_of_user_input = 0;
+  while ((c = getchar()) != EOF && c != '\n') {
+    if (length_of_user_input < stride - 1) {
+      base_pointer[(id * stride) + length_of_user_input] = c;
+      ++length_of_user_input;
+    }
+  }
+}
 
 int main() {
   int fd_name = open("names.bin", O_RDWR | O_CREAT, 0666);
   int fd_phone = open("phone.bin", O_RDWR | O_CREAT, 0666);
   int fd_address = open("address.bin", O_RDWR | O_CREAT, 0666);
+  int fd_id = open("id.bin", O_RDWR | O_CREAT, 0666);
 
-  if (ftruncate(fd_name, PAGE_SIZE) == FAILED) {
+  if (ftruncate(fd_name, NAME_PAGE_SIZE) == FAILED) {
     perror("ftruncate failed");
     close(fd_name);
+    exit(1);
   }
-  if (ftruncate(fd_phone, PAGE_SIZE) == FAILED) {
+  if (ftruncate(fd_phone, PHONE_PAGE_SIZE) == FAILED) {
     perror("ftruncate failed");
     close(fd_phone);
+    exit(1);
   }
-  if (ftruncate(fd_address, PAGE_SIZE) == FAILED) {
+  if (ftruncate(fd_address, ADDRESS_PAGE_SIZE) == FAILED) {
     perror("ftruncate failed");
     close(fd_address);
+    exit(1);
+  }
+  if (ftruncate(fd_id, ID_PAGE_SIZE) == FAILED) {
+    perror("ftruncate failed");
+    close(fd_id);
+    exit(1);
   }
 
-  char *user_name =
-      mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd_name, 0);
-  char *user_phone =
-      mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd_phone, 0);
-  char *user_address =
-      mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd_address, 0);
+  char *user_name = mmap(NULL, NAME_PAGE_SIZE, PROT_READ | PROT_WRITE,
+                         MAP_SHARED, fd_name, 0);
+  char *user_phone = mmap(NULL, PHONE_PAGE_SIZE, PROT_READ | PROT_WRITE,
+                          MAP_SHARED, fd_phone, 0);
+  char *user_address = mmap(NULL, ADDRESS_PAGE_SIZE, PROT_READ | PROT_WRITE,
+                            MAP_SHARED, fd_address, 0);
+  long *user_id =
+      mmap(NULL, ID_PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd_id, 0);
 
   if (user_name == MAP_FAILED) {
     perror("mmap");
@@ -43,6 +75,32 @@ int main() {
   }
   if (user_address == MAP_FAILED) {
     perror("mmap");
+  }
+  if (user_id == MAP_FAILED) {
+    perror("mmap");
+  }
+
+  long current_id = *user_id;
+  printf("Your user id/position is: %ld\n", current_id);
+  printf("Enter your name\n");
+  write_to_db(user_name, id, STRIDE_NAME);
+  printf("Enter your phone number\n");
+  write_to_db(user_phone, id, STRIDE_NAME);
+  printf("Enter your address\n");
+  write_to_db(user_address, id, STRIDE_NAME);
+
+  *user_id = current_id + 1;
+  if (munmap(user_name, NAME_PAGE_SIZE) == FAILED) {
+    perror("munmap failed");
+  }
+  if (munmap(user_phone, NAME_PAGE_SIZE) == FAILED) {
+    perror("munmap failed");
+  }
+  if (munmap(user_address, NAME_PAGE_SIZE) == FAILED) {
+    perror("munmap failed");
+  }
+  if (munmap(user_id, ID_PAGE_SIZE) == FAILED) {
+    perror("munmap failed");
   }
   return 0;
 }
